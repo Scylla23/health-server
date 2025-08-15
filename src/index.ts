e8a0d0e570e7da4447daecc6087b83a133115726
Pavan Kushnure <2019bcs044@sggs.ac.in>
2025-08-15 10:16:31 +0530

    server running and docker file working

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK - Server is healthy");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+export default app;
