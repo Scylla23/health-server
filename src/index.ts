2268ca02a6da83681a3814b0a9cd105dabb0261e
Pavan Kushnure <2019bcs044@sggs.ac.in>
2025-08-15 13:06:46 +0530

    Add PostgreSQL integration and update Docker configuration - Updated .gitignore to include .env file - Enhanced index.ts to establish a PostgreSQL client connection and added a test route - Created docker-compose.yml for API and PostgreSQL service orchestration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import express from "express";
+import pg from "pg";
 
 const app = express();
+
+// Get client from pg library
+const { Client } = pg;
+
+// Initialize the client
+const client = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+// Connect to the database
+await client.connect();
+
+// Let's create a new route to test our DB connection
+app.get("/test", async (req, res) => {
+  const result = await client.query("SELECT NOW()");
+  res.send(result.rows[0]);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
